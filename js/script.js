b57aec717fcc7000ac27bb5d9b4830a08cde31bf
ankirsydii <andiniputrirosyidi@gmail.com>
2026-06-07 14:25:43 +0700

    Create modern hero section with typing animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
 console.log("Portfolio Loaded");
+
+const roles = [
+    "Web Developer",
+    "Mobile Developer",
+    "AR Enthusiast",
+    "Cloud Computing Learner"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typingElement = document.getElementById("typing");
+
+function typeEffect() {
+
+    const currentRole = roles[roleIndex];
+
+    if (!isDeleting) {
+        typingElement.textContent =
+            currentRole.substring(0, charIndex++);
+
+        if (charIndex > currentRole.length) {
+            isDeleting = true;
+            setTimeout(typeEffect, 1500);
+            return;
+        }
+    } else {
+        typingElement.textContent =
+            currentRole.substring(0, charIndex--);
+
+        if (charIndex < 0) {
+            isDeleting = false;
+            roleIndex = (roleIndex + 1) % roles.length;
+        }
+    }
+
+    setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+typeEffect();
